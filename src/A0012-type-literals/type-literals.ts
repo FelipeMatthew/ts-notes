a8c1b671aa7cf7f5d9612798f80e89146b5a6796
Felipe Matthew <felipematthewnascimento@gmail.com>
2023-10-04 17:50:25 -0300

    A0012-typeliterals

--- src/A0012-type-literals/type-literals.ts
+++ src/A0012-type-literals/type-literals.ts
@@ -1,1 +1,21 @@
 // tipo é definido pelo valor constante dele.
+
+let x = 10;
+x = 0b1010;
+
+const y = 10;
+const a = 100 as const;
+
+const person = {
+  name: 'luiz' as const,
+  lastname: 'miranda',
+};
+
+function chooseColor(color: 'red' | 'blue' | 'green'): string {
+  return color;
+}
+
+console.log(chooseColor('green'));
+
+// module mode
+export default '';
