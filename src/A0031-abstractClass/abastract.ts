b3beeb774746fdb62eefb5ef5e0a385f2980dda9
felipematthew <felipematthewnascimento@gmail.com>
2023-12-28 21:32:46 -0300

    gaming about to demonstrate about abstract class

--- src/A0031-abstractClass/abastract.ts
+++ src/A0031-abstractClass/abastract.ts
@@ -1,4 +1,9 @@
-export class Character {
+// Essa classe nao pode mais ser instanciada diretamente
+// Só via poder chamar ou guerreira ou monstro
+// Classe abstrada so é instanciada quando for extended
+export abstract class Character {
+  protected abstract emoji: string;
+
   constructor(
     protected name: string,
     protected attackPower: number,
@@ -6,21 +11,43 @@ export class Character {
   ) {}
 
   attack(character: Character): void {
-    console.log(`${this.name} is attacking`);
+    this.staff();
     character.lostHealth(this.attackPower);
   }
 
   lostHealth(_attackPower: number): void {
-    console.log(`${this.name} now has ${this.health}hp`);
     this.health -= _attackPower;
+    console.log(`${this.emoji} ${this.name} - now has ${this.health}hp`);
   }
+
+  abstract staff(): void;
+  // Precisa ser instanciado e definido em todas subclasses
 }
 
-export class Warrior extends Character {}
-export class Monster extends Character {}
+export class Warrior extends Character {
+  protected emoji = '\u{1F9DD}';
+
+  staff(): void {
+    console.log(
+      this.emoji +
+        this.name +
+        '  ' +
+        "i'm the best warrior in the world, let's gooo!!",
+    );
+  }
+}
+export class Monster extends Character {
+  protected emoji = '\u{1F9DF}';
+
+  staff(): void {
+    console.log(this.emoji + this.name + ' ' + 'guaaarrr!! Lets attack');
+  }
+}
 
 const warrior = new Warrior('Valquiria', 300, 1500);
 const monster = new Monster('Goblin', 200, 8000);
 
-console.log(warrior);
-console.log(monster);
+warrior.attack(monster);
+warrior.attack(monster);
+warrior.attack(monster);
+monster.attack(warrior);
