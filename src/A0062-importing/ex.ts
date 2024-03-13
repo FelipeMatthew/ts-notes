cee72d6c429c81c8f9ff349f7b219889d7f0fc75
felipematthew <felipematthewnascimento@gmail.com>
2024-03-13 15:49:46 -0300

    :sparkles: feat: importing js to ts

---
+++ src/A0062-importing/ex.ts
@@ -0,0 +1,3 @@
+export function sum<T>(x: number, y: number): number {
+  return x + y;
+}
---
+++ src/A0062-importing/index.ts
@@ -0,0 +1,5 @@
+import { sum } from './module';
+
+const result = sum(3, 9) as number;
+
+console.log(result);
